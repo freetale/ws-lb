9fffeee4ca5380dfe88c29b514595eb7da4a46ab
freetale <lifeistfree@gmail.com>
2023-11-25 00:23:31 +0700

    add server

--- cluster/droplet/create.ps1
+++ cluster/droplet/create.ps1
@@ -1,3 +1,3 @@
-$ip=doctl compute droplet create temp-deoplet --count 2 --size s-4vcpu-8gb --update-kubeconfig --wait --region sgp1
+$ip=doctl compute droplet create temp-deoplet --size s-2vcpu-4gb --wait --region sgp1 --image docker-20-04
 scp ../../ws-server/target/debug/ws-server root@${ip}:ws-server
 ssh root@${ip} ./ws-server
