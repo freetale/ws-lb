31fbd18b1f584e102c8fed7ca014025ee2cdf770
freetale <lifeistfree@gmail.com>
2023-11-25 13:26:35 +0700

    avaliable for test

--- cluster/locust/create.ps1
+++ cluster/locust/create.ps1
@@ -1,3 +1,4 @@
-doctl kubernetes cluster create temp-locust --count 5 --size c-4 --update-kubeconfig --wait --region sgp1
+doctl kubernetes cluster create temp-locust --count 9 --size c-4 --update-kubeconfig --wait --region sgp1
 kubectl apply -k ./kustomization/
+kubectl get svc -w
 #doctl kubernetes cluster delete temp-locust
--- ws-server/src/main.rs
+++ ws-server/src/main.rs
@@ -1,31 +1,54 @@
-use rand::prelude::*;
-use std::net::TcpListener;
-use std::thread::spawn;
-use tungstenite::accept;
+//! A simple echo server.
+//!
+//! You can test this out by running:
+//!
+//!     cargo run --example echo-server 127.0.0.1:12345
+//!
+//! And then in another window run:
+//!
+//!     cargo run --example client ws://127.0.0.1:12345/
+//!
+//! Type a message into the client window, press enter to send it and
+//! see it echoed back.
 
-/// A WebSocket echo server
-fn main() {
+use std::{env, io::Error};
+
+use futures_util::{future, StreamExt, TryStreamExt};
+use log::info;
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let _ = env_logger::try_init();
+    let addr = env::args().nth(1).unwrap_or_else(|| "0.0.0.0:9001".to_string());
+
+    // Create the event loop and TCP listener we'll accept connections on.
+    let try_socket = TcpListener::bind(&addr).await;
+    let listener = try_socket.expect("Failed to bind");
+    info!("Listening on: {}", addr);
     println!("starting");
-    let server = TcpListener::bind("0.0.0.0:9001").unwrap();
-    for stream in server.incoming() {
-        spawn(move || {
-            let usteam = stream.unwrap();
-            let addr: std::net::SocketAddr = usteam.peer_addr().unwrap();
-            // println!("{}", addr);
-            let mut websocket = accept(usteam).unwrap();
-            loop {
-                let msg = match websocket.read() {
-                    Ok(x) => x,
-                    Err(_) => break,
-                };
-                // if msg.is_text() {
-                //     println!("{}", msg.to_string())
-                // }
-                // We do not want to send back ping/pong messages.
-                if msg.is_binary() || msg.is_text() {
-                    websocket.send(msg).unwrap();
-                }
-            }
-        });
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(accept_connection(stream));
     }
+
+    Ok(())
+}
+
+async fn accept_connection(stream: TcpStream) {
+    let addr = stream.peer_addr().expect("connected streams should have a peer address");
+    info!("Peer address: {}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+
+    info!("New WebSocket connection: {}", addr);
+
+    let (write, read) = ws_stream.split();
+    // We should not forward messages other than text or binary.
+    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
+        .forward(write)
+        .await
+        .expect("Failed to forward messages")
 }
