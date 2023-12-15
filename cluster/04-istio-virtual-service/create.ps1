fc2d799f3c014caba1688b5d5ccd7a6f6ea9a905
freetale <lifeistfree@gmail.com>
2023-12-15 23:19:13 +0700

    add istio

--- cluster/04-istio-virtual-service/create.ps1
+++ cluster/04-istio-virtual-service/create.ps1
@@ -1,4 +1,7 @@
-doctl kubernetes cluster create lb-ingress --count 1 --size s-2vcpu-4gb  --update-kubeconfig --wait --region sgp1
+doctl kubernetes cluster create lb-ingress --count 1 --size s-4vcpu-8gb  --update-kubeconfig --wait --region sgp1
+istioctl install -y
+kubectl label namespace default istio-injection=enabled
+
 kubectl apply -k ./kustomization/
-kubectl get svc -w
+kubectl get svc -w -A
 #doctl kubernetes cluster delete lb-ingress
